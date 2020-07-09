4674ebf5945af652bc40f37e702b14fcd1f73df8
paola nahomi hinojosa lozano <paolahonojosalz@gmail.com>
2020-07-09 10:36:03 -0500

    programa 2 finalizado

--- prog 2.py
+++ prog 2.py
@@ -1,9 +1,10 @@
 print(f'{"programa 2":_^80}')
-suma = 0
+
 num1 = int(input("ingrese numero 1: "))
 num2 = int(input("ingrese numero 2: "))
-num1 = num1 + 1
-for suma in range(num1+num2):
-    suma += num1 + num2
+suma = 0
+for numero in range(num1,num2 +1):
+    suma += numero
 print(suma)
+
 
